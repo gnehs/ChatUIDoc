7aced409dcced74544f72987a2126db8988655e8
GNEHS <jayuiop@gmail.com>
2018-05-13 14:12:32 +0800

    gulp ㄛ

--- docs/js/doc.js
+++ docs/js/doc.js
@@ -1,9 +1,11 @@
  window.onload = () => {
      typingCarousel()
-
      d = new Date();
      var hour = d.getHours() < 10 ? '0' + d.getHours() : d.getHours()
      var minutes = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes()
+     document.querySelectorAll("pre code").forEach(function(node) {
+         hljs.highlightBlock(node);
+     });
      document.querySelectorAll("header message .item").forEach(function(node) {
          node.innerHTML = hour + ':' + minutes;
      });
---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var notify = require('gulp-notify');
+var plumber = require('gulp-plumber');
+var connect = require('gulp-connect');
+
+var paths = {
+    // css
+    sassWatch: './docs/scss/**/*.scss',
+    css: './docs/css', //儲存到根目錄底下的css資料夾
+    // html
+    pugWatch: './pug/**/*.pug',
+    html: 'docs', //儲存到根目錄底下的html資料夾
+    // js
+    jsWatch: './docs/js/**/*.js',
+    // html
+    htmlWatch: './docs/**/*.html'
+};
+
+gulp.task('pug', function() {
+    return gulp.src(paths.pugWatch)
+        .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
+        .pipe(rename(function(path) {
+            var filename = path.basename.split('_')[1];
+            if (!filename) {
+                return path;
+            }
+            path.basename = filename;
+            return path;
+        }))
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest(paths.html))
+});
+
+gulp.task('sass', function() {
+    return gulp.src(paths.sassWatch)
+        .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
+        .pipe(sass({ outputStyle: 'expanded' }))
+        .pipe(gulp.dest(paths.css))
+        .pipe(connect.reload());
+});
+
+gulp.task('js', function() {
+    return gulp.src(paths.jsWatch)
+        .pipe(connect.reload());
+});
+gulp.task('html', function() {
+    return gulp.src(paths.htmlWatch)
+        .pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+    gulp.watch(paths.pugWatch, ['pug']);
+    gulp.watch(paths.sassWatch, ['sass']);
+    gulp.watch(paths.jsWatch, ['js']);
+    gulp.watch(paths.htmlWatch, ['html']);
+});
+
+gulp.task('server', function() {
+    connect.server({
+        root: 'docs',
+        port: 8880,
+        livereload: true
+    });
+    console.log('sever: http://localhost:' + 8880)
+});
+gulp.task('default', ['watch', 'pug', 'server']);
