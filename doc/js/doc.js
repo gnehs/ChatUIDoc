f8f480400a9d18d293f04c4fc9ff9a5bce428a8f
GNEHS <jayuiop@gmail.com>
2018-05-13 12:47:15 +0800

    先推

---
+++ doc/js/doc.js
@@ -0,0 +1,10 @@
+ window.onload = () => {
+     typingCarousel()
+
+     d = new Date();
+     var hour = d.getHours() < 10 ? '0' + d.getHours() : d.getHours()
+     var minutes = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes()
+     document.querySelectorAll("header message .item").forEach(function(node) {
+         node.innerHTML = hour + ':' + minutes;
+     });
+ };
