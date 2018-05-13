dbfef788331a1ba0fd47f124bd9cf2f06011deb7
GNEHS <jayuiop@gmail.com>
2018-05-13 18:55:49 +0800

    google/code-prettify

--- docs/js/doc.js
+++ docs/js/doc.js
@@ -3,9 +3,6 @@
      d = new Date();
      var hour = d.getHours() < 10 ? '0' + d.getHours() : d.getHours()
      var minutes = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes()
-     document.querySelectorAll("pre code").forEach(function(node) {
-         hljs.highlightBlock(node);
-     });
      document.querySelectorAll("header .message .item").forEach(function(node) {
          node.innerHTML = hour + ':' + minutes;
      });
